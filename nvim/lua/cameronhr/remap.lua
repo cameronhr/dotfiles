f394827025e3d2b0083ca0a5bd714bbe435ac6e1
cameronhr <camhard@gmail.com>
2025-01-06 18:33:47 -0500

    Add keymaps for Telescope

--- nvim/lua/cameronhr/remap.lua
+++ nvim/lua/cameronhr/remap.lua
@@ -1,3 +1,9 @@
 vim.g.mapleader = " "  -- Set leader key to space
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 print("Leader set to: " .. vim.inspect(vim.g.mapleader))
+
+-- Lazy-load Telescope in keymap
+vim.keymap.set('n', '<leader>ff', function() require('telescope.builtin').find_files() end, {})
+vim.keymap.set('n', '<leader>fg', function() require('telescope.builtin').live_grep() end, {})
+vim.keymap.set('n', '<leader>fb', function() require('telescope.builtin').buffers() end, {})
+vim.keymap.set('n', '<leader>fh', function() require('telescope.builtin').help_tags() end, {})
