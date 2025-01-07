607bd0d958340af249bc177ab0d6fddaf166012a
cameronhr <camhard@gmail.com>
2025-01-07 11:27:38 -0500

    Add remaps for pane navigation

--- nvim/lua/cameronhr/remap.lua
+++ nvim/lua/cameronhr/remap.lua
@@ -2,6 +2,11 @@ vim.g.mapleader = " "  -- Set leader key to space
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 print("Leader set to: " .. vim.inspect(vim.g.mapleader))
 
+vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
+
 -- Lazy-load Telescope in keymap
 vim.keymap.set('n', '<leader>ff', function() require('telescope.builtin').find_files() end, {})
 vim.keymap.set('n', '<leader>fg', function() require('telescope.builtin').live_grep() end, {})
