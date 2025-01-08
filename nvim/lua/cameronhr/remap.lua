1d5555c6543900e4a5b3e317d876e8c688986605
cameronhr <camhard@gmail.com>
2025-01-08 15:23:49 -0500

    Add persistent visual selection

--- nvim/lua/cameronhr/remap.lua
+++ nvim/lua/cameronhr/remap.lua
@@ -2,6 +2,10 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Keep visual selection after indent/dedent
+vim.api.nvim_set_keymap("v", "<", "<gv", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", ">", ">gv", { noremap = true, silent = true })
+
 vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
