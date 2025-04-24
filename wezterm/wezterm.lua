69dfe7c7d4e42b9824a7140bca99dfa9913124e4
cameronhr <camhard@gmail.com>
2025-04-24 11:58:23 -0400

    Tweak wezterm and tmux configs

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -11,59 +11,63 @@ config.font_size = 14
 config.front_end = "WebGpu"
 
 config.keys = {
-    {
-        key = "Enter",
-        mods = "CMD",
-        action = wezterm.action.ToggleFullScreen,
-    },
-    {
-        key = "w",
-        mods = "CMD",
-        action = wezterm.action.CloseCurrentPane({ confirm = false }),
-    },
+	{
+		key = "Enter",
+		mods = "CMD",
+		action = wezterm.action.ToggleFullScreen,
+	},
+	{
+		key = "w",
+		mods = "CMD",
+		action = wezterm.action.CloseCurrentPane({ confirm = false }),
+	},
 }
 
 config.window_padding = {
-    left = 40,
-    right = 40,
-    top = 40,
-    bottom = 40,
+	left = 40,
+	right = 40,
+	top = 40,
+	bottom = 40,
 }
 
 config.window_decorations = "RESIZE"
 
-function recompute_padding(window)
-    local window_dims = window:get_dimensions()
-    local overrides = window:get_config_overrides() or {}
+config.scrollback_lines = 300000
 
-    if window_dims.is_full_screen then
-        overrides.window_padding = {
-            -- top = 70,
-        }
-    end
-    window:set_config_overrides(overrides)
+function recompute_padding(window)
+	local window_dims = window:get_dimensions()
+	local overrides = window:get_config_overrides() or {}
+
+	if window_dims.is_full_screen then
+		overrides.window_padding = {
+			-- top = 70,
+		}
+	end
+	window:set_config_overrides(overrides)
 end
 
 wezterm.on("window-resized", function(window, pane)
-    recompute_padding(window)
+	recompute_padding(window)
 end)
 
 function scheme_for_appearance(appearance)
-    if appearance:find("Dark") then
-        return "Tokyo Night Storm"
-    else
-        return "Tokyo Night Day"
-    end
+	if appearance:find("Dark") then
+		return "Tokyo Night Storm"
+	else
+		return "Tokyo Night Day"
+	end
 end
 
 wezterm.on("window-config-reloaded", function(window, pane)
-    local overrides = window:get_config_overrides() or {}
-    local appearance = window:get_appearance()
-    local scheme = scheme_for_appearance(appearance)
-    if overrides.color_scheme ~= scheme then
-        overrides.color_scheme = scheme
-        window:set_config_overrides(overrides)
-    end
+	local overrides = window:get_config_overrides() or {}
+	local appearance = window:get_appearance()
+	local scheme = scheme_for_appearance(appearance)
+	if overrides.color_scheme ~= scheme then
+		overrides.color_scheme = scheme
+		window:set_config_overrides(overrides)
+	end
 end)
+
+config.window_background_opacity = 0.95
 
 return config
